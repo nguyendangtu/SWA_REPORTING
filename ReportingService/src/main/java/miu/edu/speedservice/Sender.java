efc92e79c80271e5cee88c872d5f9a67734d81bc
nguyendangtu <nguyendangtu1987@gmail.com>
2023-03-20 15:23:45 -0500

    init project

---
+++ ReportingService/src/main/java/miu/edu/speedservice/Sender.java
@@ -0,0 +1,19 @@
+package miu.edu.speedservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Sender {
+    @Autowired
+    private KafkaTemplate<String, SpeedRecord> kafkaTemplate;
+    
+    @Value("${app.topic.tofasttopic}")
+    private String topic;
+
+    public void send(SpeedRecord speedRecord){
+        kafkaTemplate.send(topic, speedRecord);
+    }
+}
---
+++ ReportingService/src/main/java/miu/edu/speedservice/SpeedRecord.java
@@ -0,0 +1,38 @@
+package miu.edu.speedservice;
+
+import java.io.Serializable;
+
+public class SpeedRecord implements Serializable {
+	public String licencePlate;
+	public int speed;
+
+	public SpeedRecord() {
+		super();
+	}
+
+	public SpeedRecord(String licencePlate, int speed) {
+		super();
+		this.licencePlate = licencePlate;
+		this.speed = speed;
+	}
+
+	public String toString() {
+		return licencePlate +" speed = "+speed;
+	}
+	public String getLicencePlate() {
+		return licencePlate;
+	}
+
+	public void setLicencePlate(String licencePlate) {
+		this.licencePlate = licencePlate;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+}
