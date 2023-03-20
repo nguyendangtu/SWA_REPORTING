6ea9cc62c44c6d345ebfefcbd17db605973d2c34
Quoc Nam Le <QNLe@miu.edu>
2023-03-20 17:28:48 -0500

    Add eureka to Reporting.

--- ReportingService/src/main/java/miu/edu/swa/pproject/report/ReportingServiceApplication.java
+++ ReportingService/src/main/java/miu/edu/swa/pproject/report/ReportingServiceApplication.java
@@ -2,12 +2,13 @@ package miu.edu.swa.pproject.report;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableKafka
-@EnableScheduling
+@EnableEurekaClient
 public class ReportingServiceApplication {
 
     public static void main(String[] args) {
