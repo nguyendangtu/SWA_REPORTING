1353203ce4416e8e7a0cf9e7741b3ee700600725
Tung Le <thanhtung2501@gmail.com>
2023-03-22 15:48:02 -0500

    ui

---
+++ SWAReport/src/app/components/line-chart/report.ts
@@ -0,0 +1,23 @@
+export interface Report {
+  topicName: string;
+  data: ReportObj[]; 
+}
+
+export interface ReportObj {
+  id: number;
+  value: number;
+  timestamp: number;
+}
+
+export interface ChartModel {
+  type: string;
+  name: string;
+  showInLegend: boolean;
+  yValueFormatString: string;
+  dataPoints: ChartDTO[];
+}
+
+export interface ChartDTO {
+  x: number;
+  y: number;
+}
---
+++ SWAReport/src/app/components/material/dropdown-list/dropdown-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { ReportService } from './../../line-chart/report.service';
+
+@Component({
+  selector: 'dropdown-list',
+  templateUrl: './dropdown-list.component.html',
+  styleUrls: ['./dropdown-list.component.css'],
+  providers: [ReportService]
+})
+export class DropdownListComponent implements OnInit {
+  topics: string[] = ["Choose topic name"];
+  chooseTopic: string = "";
+
+  @Output()
+  getTopicName: EventEmitter<string> = new EventEmitter<string>();
+
+  ngOnInit(): void {
+    this.getTopics();
+    this.chooseTopic = this.topics[0];
+  }
+
+  constructor(private reportService: ReportService) {
+  }
+
+  getTopics(): void {
+    this.reportService.getTopics().subscribe(data => {
+      this.topics.push(...data);
+    });
+  }
+
+  getTopicChange(event: any) {
+    if(event == this.topics[0]){
+      this.getTopicName.emit();
+    } else {
+      this.getTopicName.emit(event.value);
+    }
+    
+    
+  }
+
+}
